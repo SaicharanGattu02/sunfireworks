0d386fda240d61fc5a2c5445b22ff5f03dec2c33
Dell <saicharan.teamup@gmail.com>
2025-07-18 11:34:22 +0530

    committed by charan

---
+++ lib/Presentation/Authentication/SignInWithMobile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignInWithMobile extends StatefulWidget {
+  const SignInWithMobile({super.key});
+
+  @override
+  State<SignInWithMobile> createState() => _SignInWithMobileState();
+}
+
+class _SignInWithMobileState extends State<SignInWithMobile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
